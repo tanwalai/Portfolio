8fe44ae20ef6fa5e4aad9fc150f3bb4d666d8449
Tanwalai Yoongkieo <109836508+tanwalai@users.noreply.github.com>
2022-09-20 12:19:02 +0700

    Update Rock_Paper_Scissors_Game.R

--- R/Rock_Paper_Scissors_Game.R
+++ R/Rock_Paper_Scissors_Game.R
@@ -3,33 +3,41 @@ games <- function() {
   win <- 0
   lose <- 0
   firsttime <- 0
-  quit <- "exit"
   while (TRUE) {
-  while(firsttime<1){print("Welcome to Rock-Paper-Scissors Games! You can quit this game by type exit")
-    firsttime<-firsttime+1
-  }
-  user_ans <- tolower(readline("Choose your action: "))
-  action <- c("rock", "paper", "scissors")
-  result <- c(win, lose, ties)
-  names(result) <- c("win", "lose", "ties") 
-  if(user_ans == quit){
-    print("Thank you for playing! This is your score ")
-    print(result)
-    break
-  } else if(user_ans == sample(action, 1)){
-    print("ties") 
-    ties <- ties + 1
-  } else if(user_ans == "rock" & sample(action, 1) == "scissors" ){
-    print("win")
-    win <- win+1
-  } else if(user_ans == "scissors" & sample(action, 1) == "paper"){
-    print("win")
-    win <- win+1
-  } else if(user_ans == "paper" & sample(action, 1) == "rock"){
-    print("win")
-    win <- win+1
-  } else {
-    print("lose")
-    lose <- lose+1
-  }
-}}
+    while(firsttime<1){print("Welcome to Rock-Paper-Scissors Games! You can quit this game by type exit")
+      firsttime<-firsttime+1
+    }
+    user_ans <- tolower(readline("Choose your action: "))
+    action <- c("rock", "paper", "scissors")
+    result <- c(win, lose, ties)
+    names(result) <- c("win", "lose", "ties") 
+    bot_action <- sample(action, 1)
+    if(user_ans == "exit"){
+      print("Thank you for playing! This is your score ")
+      print(result)
+      break
+    } else if(user_ans == bot_action){
+      print("ties") 
+      ties <- ties + 1
+    } else if(user_ans == "rock" & bot_action == "scissors" ){
+      print("win")
+      win <- win+1
+    } else if(user_ans == "scissors" & bot_action == "paper"){
+      print("win")
+      win <- win+1
+    } else if(user_ans == "paper" & bot_action == "rock"){
+      print("win")
+      win <- win+1
+    } else if(user_ans == "paper" & bot_action == "scissors"){
+      print("lose")
+      lose <- lose+1
+    }else if(user_ans == "scissors" & bot_action == "rock"){
+      print("lose")
+      lose <- lose+1
+    }else if(user_ans == "rock" & bot_action == "paper"){
+      print("lose")
+      lose <- lose+1
+    }else {
+      print("please choose 'paper' or 'scissors' or 'rock'")
+    }
+  }}
