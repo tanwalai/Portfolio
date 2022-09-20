6cdc035366d34e138a0c2049c1fee549cef87544
Tanwalai Yoongkieo <109836508+tanwalai@users.noreply.github.com>
2022-09-20 11:36:38 +0700

    Create Rock-Paper-Scisscors Game.py

---
+++ Python/Rock-Paper-Scisscors Game.py
@@ -0,0 +1,32 @@
+def game():
+    first_time = 0
+    win = 0
+    lose = 0
+    ties = 0
+    score = {
+        "win" : win,
+        "lose" : lose,
+        "ties" : ties
+    }
+    bot_list = ['rock', 'paper', 'scissors']
+    while True :
+        while first_time == 0:
+            print("Welcome to Rock-Paper-Scissors game!\n You can quit this game by type exit")
+            first_time += 1
+        user_ans = input()
+        bot_action = random.choice(bot_list)
+        if user_ans == "exit":
+            print("Thanks for playing my game! This is your score")
+            print(score)
+            break
+        elif user_ans == bot_action :
+            print("ties")
+            score["ties"] +=1
+        elif (user_ans == "rock" and bot_action == "scissors") or \
+        (user_ans == "scissors" and bot_action == "paper") or \
+        (user_ans == "paper" and bot_action == "rock") :
+            print("win")
+            score["win"] +=1
+        else:
+            print("lose")
+            score["lose"] +=1
